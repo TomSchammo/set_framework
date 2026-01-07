97fa37f8709e13feaa181459d39ba62720c2210e
Tom Schammo <tom@tomschammo.com>
2026-01-07 15:39:18 +0100

    add benchmark script

---
+++ bench.py
@@ -0,0 +1,41 @@
+from utils import MagnitudeSET
+from set_keras import SET_MLP_CIFAR10
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('--target_accuracy', type=float, required=True)
+parser.add_argument('--max_epochs', type=int, required=True)
+
+args = parser.parse_args()
+
+print(
+    f"---- Training until convergence to {args.target_accuracy} accuracy (max {args.max_epochs} epochs) ----"
+)
+
+strategies = [MagnitudeSET()]
+
+models = [SET_MLP_CIFAR10]
+
+results: list[tuple[str, int]] = []
+
+for model_cls in models:
+    for strategy in strategies:
+        model = model_cls(strategy=strategy, max_epochs=args.max_epochs)
+
+        epoch_count = model.train(target_accuracy=args.target_accuracy)
+        print(f"took {epoch_count} epochs until convergance")
+        results.append(
+            (f"{model.__class__.__name__} + {strategy.__class__.__name__}",
+             epoch_count))
+
+# Pretty print results in a table
+print("\n" + "=" * 60)
+target_accuracy = args.target_accuracy
+print(
+    f"{'Model + Strategy (' + str(target_accuracy * 100) + '%)':<40} | {'Epochs':>7}"
+)
+print("-" * 60)
+for name, epoch_count in results:
+    print(f"{name:<40} | {epoch_count:>7}")
+print("=" * 60)
