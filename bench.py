123f2939c422f597324705c1265b153d8d73bd58
Tom Schammo <tom@tomschammo.com>
2026-01-10 12:07:57 +0100

    refactor: Move main code in main function

--- bench.py
+++ bench.py
@@ -12,57 +12,66 @@ parser.add_argument('--accuracy',
                     required=False)
 parser.add_argument('--time', type=bool, action="store_true", required=False)
 
-args = parser.parse_args()
 
-print(
-    f"---- Training until convergence to {args.target_accuracy} accuracy (max {args.max_epochs} epochs) ----"
-)
+def main():
 
-assert args.accuracy != args.time, "Expected either 'accuracy' or 'time'"
+    args = parser.parse_args()
 
-if args.time:
-    assert args.target_accuracy, "You forgot to set a target_accuracy!"
-
-if args.accuracy and args.target_accuracy:
     print(
-        f"\033[33m[Warning]: You set a target_accuracy ({args.target_accuracy}) but are not benchmarking convergance time! Target accuracy will have no effect!\033[0m"
+        f"---- Training until convergence to {args.target_accuracy} accuracy (max {args.max_epochs} epochs) ----"
     )
 
-target_accuracy = 1.0 if args.accuracy else args.target_accuracy
-max_epochs = args.max_epochs
+    assert args.accuracy != args.time, "Expected either 'accuracy' or 'time'"
 
-strategies = [RandomSET()]
+    if args.time:
+        assert args.target_accuracy, "You forgot to set a target_accuracy!"
 
-models = [SET_MLP_CIFAR10]
+    if args.accuracy and args.target_accuracy:
+        print(
+            f"\033[33m[Warning]: You set a target_accuracy ({args.target_accuracy}) but are not benchmarking convergance time! Target accuracy will have no effect!\033[0m"
+        )
 
-results: list[tuple[str, int]] = []
+    target_accuracy = 1.0 if args.accuracy else args.target_accuracy
+    max_epochs = args.max_epochs
 
-for model_cls in models:
-    for strategy in strategies:
-        model = model_cls(strategy=strategy, max_epochs=max_epochs)
+    strategies = [RandomSET()]
 
-        epoch_count, best_accuracy = model.train(
-            target_accuracy=target_accuracy)
-        if args.time:
-            print(f"took {epoch_count} epochs until convergance")
-            results.append((f"{strategy.__class__.__name__}", epoch_count))
-        else:
-            print(
-                f"Reached {best_accuracy*100}% accuracy after {max_epochs} epochs"
-            )
-            results.append(
-                (f"{strategy.__class__.__name__}", int(best_accuracy * 100)))
+    models = [SET_MLP_CIFAR10]
+
+    results: list[tuple[str, int]] = []
+
+    for model_cls in models:
+        for strategy in strategies:
+            model = model_cls(strategy=strategy, max_epochs=max_epochs)
+
+            epoch_count, best_accuracy = model.train(
+                target_accuracy=target_accuracy)
+            if args.time:
+                print(f"took {epoch_count} epochs until convergance")
+                results.append((f"{strategy.__class__.__name__}", epoch_count))
+            else:
+                print(
+                    f"Reached {best_accuracy*100}% accuracy after {max_epochs} epochs"
+                )
+                results.append((f"{strategy.__class__.__name__}",
+                                int(best_accuracy * 100)))
+
 
 # Pretty print results in a table
-print("\n" + "=" * 60)
-if args.time:
-    print(
-        f"{'Strategy (' + str(target_accuracy * 100) + '%)':<40} | {'Epochs':>7}"
-    )
-else:
-    print(f"{'Strategy (' + str(max_epochs) + 'epochs)':<40} | {'Epochs':>7}")
+    print("\n" + "=" * 60)
+    if args.time:
+        print(
+            f"{'Strategy (' + str(target_accuracy * 100) + '%)':<40} | {'Epochs':>7}"
+        )
+    else:
+        print(
+            f"{'Strategy (' + str(max_epochs) + 'epochs)':<40} | {'Epochs':>7}"
+        )
 
-print("-" * 60)
-for name, metric in results:
-    print(f"{name:<40} | {metric:>7}")
-print("=" * 60)
+    print("-" * 60)
+    for name, metric in results:
+        print(f"{name:<40} | {metric:>7}")
+    print("=" * 60)
+
+if __name__ == "__main__":
+    main()
